7e4c4beecb15ff05947c047d7bf7c100b6cedc44
itsvra <vardhan16@hotmail.com>
2017-10-17 11:33:55 +0200

    Update delete_download_folder.py
    simplified code to delete all the files in a particular directory and file extension

--- delete_download_folder.py
+++ delete_download_folder.py
@@ -8,3 +8,12 @@ for the_file in os.listdir(folder):
         elif os.path.isdir(file_path): shutil.rmtree(file_path)
     except Exception as e:
         print(e)
+        
+        
+#simplified code is below:        
+        
+import glob, os.path
+
+filelist = glob.glob(os.path.join('C:\\Users\\vra\\Downloads', "*.tcmx"))
+for f in filelist:
+    os.remove(f)
