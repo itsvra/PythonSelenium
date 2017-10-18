3aaa3e77272ee9dabdba96d58db6004822739672
itsvra <vardhan16@hotmail.com>
2017-10-18 11:43:52 +0200

    Update delete_download_folder.py
    added few lines of code

--- delete_download_folder.py
+++ delete_download_folder.py
@@ -1,3 +1,5 @@
+# this will delete all the file in the directory. it also deletes subdirectories 
+
 import os, shutil
 folder = r'C:\Users\vra\Downloads'
 for the_file in os.listdir(folder):
@@ -10,10 +12,20 @@ for the_file in os.listdir(folder):
         print(e)
         
         
-#simplified code is below:        
+#simplified code is below: this will delete only a file will with certain extension       
         
 import glob, os.path
 
 filelist = glob.glob(os.path.join('C:\\Users\\vra\\Downloads', "*.tcmx"))
 for f in filelist:
     os.remove(f)
+
+# to check if the file exist and rasie exception use
+
+import glob
+
+if glob.glob('C:\\Program Files (x86)\\Google\\Chrome\\Application\\62.0.3202.62\\*.tcmx'):
+            print 'The file exists, Ocular File is successfully downloaded'
+
+ else:
+       raise Exception ("No file exists, there might be some problem downloading the file!")
