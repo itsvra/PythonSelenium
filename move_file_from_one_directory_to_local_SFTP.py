8ae933dcc43d3e46b472c1456b5df2ddd02f6828
itsvra <vardhan16@hotmail.com>
2017-10-25 13:05:02 +0200

    Add files via upload
    SFTP operations using python

---
+++ move_file_from_one_directory_to_local_SFTP.py
@@ -0,0 +1,22 @@
+import pysftp
+
+remote_file = 'zmd-backend.log'
+srv = pysftp.Connection(host="herkules", username="vra",
+password="gj1017")
+
+data = srv.listdir('/var/log')
+
+for i in data:
+    print i
+
+remotepath = '/var/log/2011-10-04SyslogCatchAll.txt'  #file in sftp
+localpath = 'C:/Users/vra/2011-10-04SyslogCatchAll.txt' #moves it to this location
+
+srv.get(remotepath, localpath)
+
+f = open(r"C:\Users\vra\2011-10-04SyslogCatchAll.txt")
+
+print f.readlines(1) #reads the first line of file
+
+srv.close()
+
